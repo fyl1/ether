cf0efb92f50781023a90f7fd6255432563acefc1
fyl <97fyl97@gmail.com>
2022-08-28 22:33:35 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   // burgerBtn.addEventListener('click', ()=>{
   //   menu.classList.toggle('menu--open');
   // });
-  
+
   burgerBtn.forEach((item) => {
     item.addEventListener("click", function () {
       this.parentNode.classList.toggle("menu--open")
@@ -26,19 +26,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   });
   openLink.forEach((item) => {
+    console.log("тут вообеще что-то происходит 1")
     item.addEventListener("click", function () {
-      menu.forEach((item2) => {
-        item2.addEventListener("click", function () {
-          item2.classList.remove("menu--open")
-          // document.body.style.overflow = 'hidden';
-          // if (this.parentNode.contains != "menu--open") {
-          //   console.log(this.parentNode.contains)
-          //   document.body.style.overflow = 'inherit';
-          // }
-        });
-      // }
+      console.log("тут вообеще что-то происходит 2")
+      for (var i = 0; i < menu.length; i++) {
+          menu[i].classList.remove('menu--open');
+          menu[i].onclick = function (event) {
+              console.log("ONCLICK");
+              //remove all active class
+              removeClass();
+          }
+      }
+      
+      function removeClass(){
+        for (var i = 0; i < menu.length; i++) {
+          menu[i].classList.remove('menu--open');
+        }
+      }
     });
-
   });
-});
+
 });
