55ea80da61352134b2cc1a33e5a89f3ecc6abd48
fyl <97fyl97@gmail.com>
2022-08-28 21:29:50 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const MainVideo = document.querySelector(".mein-video");
   const menu = document.querySelectorAll(".menu");
   const burgerBtn = document.querySelectorAll(".burger");
+  const secondVideo = document.querySelector(".second-video")
 
-  MainVideo.play();
+  // MainVideo.play();
+  // secondVideo.play();
+  document.querySelector(".second-video").play();
+  document.querySelector(".mein-video").play();
 
   // burgerBtn.addEventListener('click', ()=>{
   //   menu.classList.toggle('menu--open');
@@ -11,11 +15,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
   burgerBtn.forEach((item) => {
     item.addEventListener("click", function () {
       this.parentNode.classList.toggle("menu--open")
-      document.body.style.overflow = 'hidden';
-      if (this.parentNode.contains != "menu--open") {
-        console.log(this.parentNode.contains)
-        document.body.style.overflow = 'inherit';
-      }
+      // document.body.style.overflow = 'hidden';
+      // if (this.parentNode.contains != "menu--open") {
+      //   console.log(this.parentNode.contains)
+      //   document.body.style.overflow = 'inherit';
+      // }
     });
 
   });
