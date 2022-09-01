9db4bd39e35207614c10662f2f5575612f0854f4
fyl <97fyl97@gmail.com>
2022-09-01 21:21:24 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
   // secondVideo.play();
   document.querySelector(".second-video").play();
   document.querySelector(".mein-video").play();
+  document.querySelector(".sub-video-1").play();
+  document.querySelector(".sub-video-2").play();
 
   // burgerBtn.addEventListener('click', ()=>{
   //   menu.classList.toggle('menu--open');
@@ -25,30 +27,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
 
   });
-  if (menu.contains == "menu--open") {
-    console.log("тут что-то есть")
-  }
-  console.log(openLink)
-  menu.forEach((item) => {
-    item.addEventListener("click", function (e) {
-      console.log(event.target)
-      if (event.target == "main-nav__link") {
-        console.log("тут что-то есть")
-      }
-      // this.parentNode.classList.toggle("menu--open")
-      // // document.body.style.overflow = 'hidden';
-      // // if (this.parentNode.contains != "menu--open") {
-      // //   console.log(this.parentNode.contains)
-      // //   document.body.style.overflow = 'inherit';
-      // // }
-    });
-  });
-  document.querySelector(".sub-video-1").play();
-  document.querySelector(".sub-video-2").play();
-  setTimeout(() => {
-    document.querySelector(".sub-video-1").play();
-    document.querySelector(".sub-video-2").play();
-    console.log("dddaa")
-  }, 5000);
+  // if (menu.contains == "menu--open") {
+  //   console.log("тут что-то есть")
+  // }
+  // console.log(openLink)
+  // menu.forEach((item) => {
+  //   item.addEventListener("click", function (e) {
+  //     console.log(event.target)
+  //     if (event.target == "main-nav__link") {
+  //       console.log("тут что-то есть")
+  //     }
+  //     // this.parentNode.classList.toggle("menu--open")
+  //     // // document.body.style.overflow = 'hidden';
+  //     // // if (this.parentNode.contains != "menu--open") {
+  //     // //   console.log(this.parentNode.contains)
+  //     // //   document.body.style.overflow = 'inherit';
+  //     // // }
+  //   });
+  // });
+
+  // setTimeout(() => {
+  //   document.querySelector(".sub-video-1").play();
+  //   document.querySelector(".sub-video-2").play();
+  //   console.log("dddaa")
+  // }, 5000);
 
 });
