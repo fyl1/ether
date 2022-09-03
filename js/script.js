82b6b3fa6199268572ad76f9d79e58e9512dbbde
fyl <97fyl97@gmail.com>
2022-09-03 13:02:41 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
   // secondVideo.play();
   document.querySelector(".second-video").play();
   document.querySelector(".mein-video").play();
-  document.querySelector(".mein-video-1").play();
-  document.querySelector(".mein-video-2").play();
+  // document.querySelector(".mein-video-1").play();
+  // document.querySelector(".mein-video-2").play();
   // document.querySelector(".sub-video-1").play();
   // document.querySelector(".sub-video-2").play();
 
@@ -54,4 +54,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
   //   console.log("dddaa")
   // }, 5000);
 
+   setTimeout(() => {
+    let parentEl_1 = document.querySelector(".price__img--1");
+    let parentEl_2 = document.querySelector(".price__img--2");
+         img_1 = document.createElement("IMG");
+         img_1.src = "video/Фото_(front)_1.gif";
+         img_2 = document.createElement("IMG");
+         img_2.src = "video/Фото_(back)_1.gif";
+    parentEl_1.appendChild(img_1);
+    parentEl_2.appendChild(img_2);
+    console.log("dddaa")
+   }, 5000);
 });
